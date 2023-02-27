05578e9d917eab340aa88cea862c37fb86f1caf5
issei <issei.hiramatsu@gmail.com>
2023-02-27 10:22:26 +0900

    add: importer.dart

---
+++ lib/importer.dart
@@ -0,0 +1,7 @@
+//色やtextStyleなどUIなどで頻繁に使うものを格納しておく
+//あくまでUIの構築に使うのでmaterial.dartは入れない（理解を改めて理解したら変更する）
+
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:my_book_library/component/shared/token/color/color.dart';
+export 'package:my_book_library/component/shared/token/space_box/space_box.dart';
+export 'package:my_book_library/component/shared/token/text_style/text_style.dart';
