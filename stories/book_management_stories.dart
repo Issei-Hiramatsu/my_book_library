f6db6ee1af7f95806eb01e8b191adf0cf0afd9b8
issei <issei.hiramatsu@gmail.com>
2023-02-27 12:01:47 +0900

    add: monarch

---
+++ stories/book_management_stories.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:my_book_library/component/local/book_management/book_management.dart';
+
+Widget bookManagement() => MaterialApp(home: BookManagement());
